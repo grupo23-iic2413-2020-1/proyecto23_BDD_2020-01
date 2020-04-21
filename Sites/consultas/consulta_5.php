5d943a791df6c38c0a3c83daf97788a4ba616fbe
Diego Fluxá <dgfluxa@uc.cl>
2020-04-21 00:07:09 -0400

    Arreglos menores

--- Sites/consultas/consulta_5.php
+++ Sites/consultas/consulta_5.php
@@ -1,7 +1,8 @@
 <?php include('../templates/header.html');   ?>
 
 <body>
-<?php# Entregue el identificador y nombre de usuario junto a la fecha de inicio en formato
+<?php
+#Entregue el identificador y nombre de usuario junto a la fecha de inicio en formato
 #YYYY-MM-DD, la fecha de t´ermino y el nombre del hotel de las reservas que parten desde
 #el 01 de enero del 2020 y terminan antes del 31 de marzo del 2020, ambas fechas
 #inclusive.
