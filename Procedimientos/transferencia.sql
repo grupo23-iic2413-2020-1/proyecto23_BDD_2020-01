afcc719885ea2727b9067608bf74b15ab9cd7ca0
Clemente Maruri <camaruri@uc.cl>
2020-04-24 16:06:25 -0400

    transferencia

---
+++ Procedimientos/transferencia.sql
@@ -0,0 +1,14 @@
+%%sql
+
+CREATE OR REPLACE FUNCTION transferencia()
+RETURNS void AS $$
+DECLARE
+    tupla RECORD;
+    concat varchar;
+BEGIN
+    FOR tupla IN SELECT * FROM Personas LOOP
+        concat = tupla.nombre || tupla.apellido;
+        INSERT INTO PersonasCompleto VALUES (tupla.run, concat);
+    END LOOP;
+END
+$$ language plpgsql
