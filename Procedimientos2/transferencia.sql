f98677196677b885f62c943fe969892c05dccbc7
Diego Fluxá <dgfluxa@uc.cl>
2020-04-29 18:37:44 -0400

    transferencia

---
+++ Procedimientos2/transferencia.sql
@@ -0,0 +1,13 @@
+CREATE OR REPLACE FUNCTION
+transferencia ()
+RETURNS void AS $$
+DECLARE
+tupla RECORD;
+concat varchar;
+BEGIN
+FOR tupla IN SELECT * FROM Personas LOOP
+concat = tupla.nombre || tupla.apellido;
+insert into personascompleto values (tupla.rut, concat);
+END LOOP;
+END
+$$ language plpgsql
