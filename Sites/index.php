1bd23765e0704e81b617cba84bd34f9ef68e2fac
Diego Fluxá <dgfluxa@uc.cl>
2020-05-16 01:51:14 -0400

    Mejoras estéticas index

--- Sites/index.php
+++ Sites/index.php
@@ -2,15 +2,68 @@
 
 <?php include('templates/navbar.php');   ?> 
 
-
+<!-- Carousel -->
+<div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+    <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner" role="listbox" style=" width:100%; height: 764px !important;">
+    <div class="carousel-item active">
+      <img src="img/carousel/slide_welcome.jpg" style=" width:100%; height: 764px">
+      <div class="carousel-caption d-none d-md-block " >
+        <div class="card bg-secondary">
+          <h1>Bienvenido a Splinter S.A</h1>
+          <p>Aquí encontrarás todo lo necesario para realizar el viaje artístico de tu vida</p>
+          <a role="button" href="#" class="btn btn-success">Ver más</a>
+        </div>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="img/carousel/slide_itinerario.jpg" style=" width:100%; height: 764px">
+      <div class="carousel-caption d-none d-md-block">
+        <div class="card bg-secondary">
+          <h1>¡Crea un itinerario!</h1>
+          <p>Con nuestra ayuda puedes crear un itinerario de viajes para visitar todas las obras 
+            de tus artistas favoritos de manera rápida y sencilla</p>
+          <a role="button" href="#" class="btn btn-success">Ir</a>
+        </div>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="img/carousel/slide_artistas.jpg" style=" width:100%; height: 764px">
+      <div class="carousel-caption d-none d-md-block">
+        <div class="card bg-secondary">
+          <h1>Conoce a nuestros artistas</h1>
+          <p>Aquí, puedes explorar a todos los artistas disponibles para crear tu viaje, conocer sus obras, donde se presentan estas y mucho más.</p>
+          <a role="button" href="/~grupo23/consultas/artistas.php" class="btn btn-success">Ir</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+<!-- End Carousel -->
 <body class= "bg-secondary text-white">
   <div class="container">
     
     <br>
-
-    <h1 class= "text-white" align="center">Entrega 3 BDD Grupos 23 y 50 </h1>
     <br>
-    <h3 class= "text-white" align="center">Splinter S.A.</h3>
+
+    <h5 class= "text-white" align="center">Entrega 3 BDD Grupos 23 y 50 </h5>
+    <br>
+    <p class= "text-white" align="center">Sección en desarrollo</p>
+
+
+
 
     <div class="row">
       <div class="col">
