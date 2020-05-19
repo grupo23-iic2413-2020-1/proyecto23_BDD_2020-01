94c6066f42cb3ffddbffd6340f93b9fb20c0e860
Diego Fluxá <dgfluxa@uc.cl>
2020-05-19 19:17:05 -0400

    Agregadas variables en perfil

--- Sites/navegacion/perfil.php
+++ Sites/navegacion/perfil.php
@@ -11,25 +11,32 @@ $result = $db -> prepare($query);
 $result -> bindParam(1, $_SESSION['current_uid']);
 $result -> execute();
 $user = $result -> fetchAll();
+
+$uid = $user[0][0];
+$username = $user[0][1];
+$unombre = $user[0][2];
+$correo = $user[0][3];
+$udir = $user[0][4];
+
 ?> 
 
 <body class= "bg-secondary text-white">
     <div class="container">
         <div class="row justify-content-md-center">
-            <h2> Perfil de <?php echo $user[0][1] ?> </h2>
+            <h2> Perfil de <?php echo $username?> </h2>
         </div>
 
         <br>
         <div class="row justify-content-md-center">
             <div class='col-md-auto'>
                 <div>
-                    <h5><b>Nombre: </b> <?php echo $user[0][2] ?> </h5>
+                    <h5><b>Nombre: </b> <?php echo $unombre ?> </h5>
                 </div>
                 <div>
-                    <h5><b>Mail: </b> <?php echo $user[0][3] ?> </h5>
+                    <h5><b>Mail: </b> <?php echo $correo ?> </h5>
                 </div>
                 <div>
-                    <h5><b>Dirección: </b> <?php echo $user[0][4] ?> </h5>
+                    <h5><b>Dirección: </b> <?php echo $udir ?> </h5>
                 </div>
             </div>
             </div>
