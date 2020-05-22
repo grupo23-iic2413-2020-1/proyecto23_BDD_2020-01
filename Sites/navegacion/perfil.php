e7bbfceaf6028dbad9745a04534496c30251cdfc
Juan Pablo Correa <jpcorrea3@uc.cl>
2020-05-21 23:59:20 -0400

    ddv

--- Sites/navegacion/perfil.php
+++ Sites/navegacion/perfil.php
@@ -18,7 +18,7 @@ $unombre = $user[0][2];
 $correo = $user[0][3];
 $udir = $user[0][4];
 
-$query_2 = "SELECT t2.fecha_compra, t1.lnombre, t1.hora_apertura, t1.hora_cierre FROM dblink('dbname=$databaseName_2 options=-csearch_path=',
+$query_2 = "SELECT t2.fecha_compra, t1.lnombre, t1.hora_apertura, t1.hora_cierre FROM dblink('dbname=$databaseName_2',
             'SELECT m.lid, l.lnombre, m.hora_apertura, m.hora_cierre FROM Museo AS m, Lugar AS l WHERE m.lid = l.lid ')
             AS t1(lid INT, lnombre VARCHAR(255), hora_apertura TIME, hora_cierre TIME), Entradas AS t2
             WHERE t1.lid = t2.lid";
@@ -35,7 +35,6 @@ $entradas = $result_2 -> fetchAll();
 <body class= "bg-secondary text-white">
     <div class="container">
         <div class="row justify-content-md-center">
-            <h2> <?php type($entradas) ?> </h2>
             <h2> Perfil de <?php echo $username;?> </h2>
         </div>
 
