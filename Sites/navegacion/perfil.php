20de6539a1405af6da6ddfa33e0d2621bf32f332
Juan Pablo Correa <jpcorrea3@uc.cl>
2020-05-21 23:50:45 -0400

    fvfv

--- Sites/navegacion/perfil.php
+++ Sites/navegacion/perfil.php
@@ -18,7 +18,7 @@ $unombre = $user[0][2];
 $correo = $user[0][3];
 $udir = $user[0][4];
 
-$query_2 = "SELECT * dblink('dbname=$databaseName_2 options=-csearch_path=',
+$query_2 = "SELECT * FROM dblink('dbname=$databaseName_2 options=-csearch_path=',
             'SELECT m.lid, l.lnombre, m.hora_apertura, m.hora_cierre FROM Museo AS m, Lugar AS l WHERE m.lid = l.lid ')
             AS t1(lid INT, lnombre VARCHAR(255), hora_apertura TIME, hora_cierre TIME)";
 
