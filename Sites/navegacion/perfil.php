09648f4315d78f83c3443069ee00f85652313492
Clemente Maruri <camaruri@uc.cl>
2020-05-24 12:06:54 -0400

    FDSF

--- Sites/navegacion/perfil.php
+++ Sites/navegacion/perfil.php
@@ -137,7 +137,7 @@ $reservas = $result_4 -> fetchAll();
                     </button>
 
                     <!-- Modal -->
-                    <div class="modal fade" id="entradas" tabindex="-1" role="dialog" aria-labelledby="deleteModalLabel" aria-hidden="true">
+                    <div class="modal fade" id="reservas" tabindex="-1" role="dialog" aria-labelledby="deleteModalLabel" aria-hidden="true">
                     <div class="modal-dialog" role="document">
                         <div class="modal-content">
                         <div class="modal-header">
@@ -161,7 +161,7 @@ $reservas = $result_4 -> fetchAll();
 
                             <?php
                                 foreach ($reservas as $res) {
-                                echo "<tr> <td>$res[3]</td> <td>$res[4]</td>  <td>$res[2]</td></tr>";
+                                echo "<tr> <td>$res[0]</td> <td>$res[1]</td>  <td>$res[2]</td></tr>";
                             }
                             ?>
                             </tbody>
