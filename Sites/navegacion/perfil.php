41a86ebb089e01025401d030ac64552229d52646
Clemente Maruri <camaruri@uc.cl>
2020-05-24 11:32:03 -0400

    ver reservas

--- Sites/navegacion/perfil.php
+++ Sites/navegacion/perfil.php
@@ -18,10 +18,11 @@ $unombre = $user[0][2];
 $correo = $user[0][3];
 $udir = $user[0][4];
 
-$query_2 = "SELECT t2.fecha_compra, t1.lnombre, t1.hora_apertura, t1.hora_cierre FROM dblink('dbname=$databaseName_2',
+$query_2 = "SELECT t2.fecha_compra, t1.lnombre, t1.hora_apertura, t1.hora_cierre FROM 
+            dblink('dbname=grupo50e3 host=localhost port=5432 user=grupo50 password=grupo2350',
             'SELECT m.lid, l.lnombre, m.hora_apertura, m.hora_cierre FROM Museo AS m, Lugar AS l WHERE m.lid = l.lid')
             AS t1(lid INT, lnombre VARCHAR(255), hora_apertura TIME, hora_cierre TIME), Entradas AS t2
-            WHERE t2.uid = $uid";
+            WHERE t2.uid = $uid AND t1.lid = t2.lid";
 
 
 #Se prepara y ejecuta la consulta. Se obtienen TODOS los resultados
@@ -105,7 +106,7 @@ $reservas = $result_4 -> fetchAll();
 
                             <?php
                                 foreach ($entradas as $entr) {
-                                echo "<tr> <td>$entr[0]</td> <td>$entr[1]</td> <td>$entr[2]</td> <td>$entr[3]</td></tr>";
+                                echo "<tr> <td>$entr[3]</td> <td>$entr[4]</td> <td>$entr[2]</td></tr>";
                             }
                             ?>
                             </tbody>
