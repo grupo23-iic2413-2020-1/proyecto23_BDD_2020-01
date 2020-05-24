9c2bd04b468ad4cb74185d1399a0bc2555aeab86
felipe lira larrain <flira4@uc.cl>
2020-05-24 00:17:19 -0400

    AAA

--- Sites/navegacion/eleccion_ticket.php
+++ Sites/navegacion/eleccion_ticket.php
@@ -14,8 +14,8 @@
 
   $query = "SELECT Origen.did, Origen.cnombre, Destino.cnombre, Origen.salida, Origen.duracion, 
             Origen.medio, Origen.max, Origen.precio, Origen.cid1, Origen.cid2
-            FROM (SELECT * FROM Destinos, Ciudades WHERE Ciudades.cnombre=$ciudad_origen AND Ciudades.cid=Destinos.cid1) AS Origen, 
-            (SELECT * FROM Destinos, Ciudades WHERE Ciudades.cnombre=$ciudad_destino AND Ciudades.cid=Destinos.cid2) AS Destino WHERE 
+            FROM (SELECT * FROM Destinos, Ciudades WHERE Ciudades.cnombre='$ciudad_origen' AND Ciudades.cid=Destinos.cid1) AS Origen, 
+            (SELECT * FROM Destinos, Ciudades WHERE Ciudades.cnombre='$ciudad_destino' AND Ciudades.cid=Destinos.cid2) AS Destino WHERE 
             Origen.did = Destino.did";
 
   #Se prepara y ejecuta la consulta. Se obtienen TODOS los resultados
