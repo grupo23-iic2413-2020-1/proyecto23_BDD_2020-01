39ffee8a50ae714a224e5a3467884438fc4086f4
Juan Pablo Correa <jpcorrea3@uc.cl>
2020-05-24 17:56:42 -0400

    tick

--- Sites/navegacion/eleccion_ticket.php
+++ Sites/navegacion/eleccion_ticket.php
@@ -14,15 +14,23 @@ include('../templates/navbar.php');   ?>
   $ciudad_destino = $_POST["ciudad_destino"];
 
   $query = "SELECT Origen.did, Origen.cnombre, Destino.cnombre, Origen.salida, Origen.duracion, 
-            Origen.medio, Origen.max, Origen.precio, Origen.cid1, Origen.cid2
+            Origen.medio, (Origen.max - Comprados.total), Origen.precio, Origen.cid1, Origen.cid2
             FROM (SELECT * FROM Destinos, Ciudades WHERE Ciudades.cnombre='$ciudad_origen' AND Ciudades.cid=Destinos.cid1) AS Origen, 
-            (SELECT * FROM Destinos, Ciudades WHERE Ciudades.cnombre='$ciudad_destino' AND Ciudades.cid=Destinos.cid2) AS Destino WHERE 
-            Origen.did = Destino.did";
+            (SELECT * FROM Destinos, Ciudades WHERE Ciudades.cnombre='$ciudad_destino' AND Ciudades.cid=Destinos.cid2) AS Destino,
+            (SELECT did, COUNT(did) AS total FROM Tickets Group by(did)) AS Comprados WHERE 
+            Origen.did = Destino.did AND Origen.did = Comprados.did";
 
   #Se prepara y ejecuta la consulta. Se obtienen TODOS los resultados
   $result = $db -> prepare($query);
 	$result -> execute();
   $destinos = $result -> fetchAll();
+
+  $query_2 = "SELECT did, COUNT(did) FROM Tickets Group by(did);";
+
+#Se prepara y ejecuta la consulta. Se obtienen TODOS los resultados
+  $result_2 = $db -> prepare($query_2);
+  $result_2 -> execute();
+  $comprados = $result_2 -> fetchAll();
 
   ?>
 
---
+++ Sites/navegacion/validacion_ticket.php
@@ -0,0 +1,96 @@
+<?php session_start();
+include('../templates/header.html');   
+include('../templates/navbar.php');  
+
+require("../config/conexion.php");
+
+
+$lid = $_GET['lid'];
+$lnombre = $_GET['lnombre'];
+$precio = $_GET['precio'];
+
+$query = "SELECT * FROM Usuarios WHERE Usuarios.uid = ?";
+
+
+#Se prepara y ejecuta la consulta. Se obtienen TODOS los resultados
+$result = $db -> prepare($query);
+$result -> bindParam(1, $_SESSION['current_uid']);
+$result -> execute();
+$user = $result -> fetchAll();
+
+$uid = $user[0][0];
+$username = $user[0][1];
+$unombre = $user[0][2];
+$correo = $user[0][3];
+$udir = $user[0][4];
+
+$query2 = "SELECT MAX(eid) FROM Entradas";
+$result_2 = $db -> prepare($query2);
+$result_2 -> execute();
+$max_eid = $result_2 -> fetchAll();
+
+if ($max_eid == NULL) {
+        $eid = 1;
+    } 
+else {$eid = $max_eid[0][0] + 1;
+    }
+
+$fecha_compra = date('Y-m-d');
+$lid_int = (int)$lid;
+$query3 = "INSERT INTO Entradas(eid, uid, lid, fecha_compra) 
+           VALUES (?, ?, ?, ?)";
+$result_3 = $db -> prepare($query3);
+$result_3 -> bindParam(1, $eid);
+$result_3 -> bindParam(2, $uid);
+$result_3 -> bindParam(3, $lid_int);
+$result_3 -> bindParam(4, $fecha_compra);
+$result_3 -> execute();
+
+?> 
+
+<body class= "bg-secondary text-white">
+    <div class="container">
+        <div class="row justify-content-md-center">
+            <h2> Tu Compra ha sido realizada con éxito </h2>
+        </div>
+        <br>
+        <div class="row justify-content-md-center">
+            <h2> Detalles compra: </h2>
+        </div>
+
+        <br>
+        <div class="row justify-content-md-center">
+            <div class='col-md-auto'>
+                <div>
+                    <h5><b>Nombre Lugar: </b> <?php echo $lnombre ?> </h5>
+                    <br>
+                </div>
+                <div>
+                    <h5><b>Usuario: </b> <?php echo $username ?> </h5>
+                    <br>
+                </div>
+                <div>
+                    <h5><b>Mail: </b> <?php echo $correo ?> </h5>
+                    <br>
+                </div>
+                <div>
+                    <h5><b>Número de Ticket: </b> <?php echo $eid ?> </h5>
+                    <br>
+                </div>
+                <div>
+                    <h5><b>Precio: </b> <?php echo $precio ?> </h5>
+                    <br>
+                </div>
+                <div>
+                    <h5><b>Fecha de Compra: </b> <?php echo $fecha_compra ?> </h5>
+                    <br>
+                </div>
+
+            </div>
+            </div>
+    </div>
+    <form align='center' action='perfil.php'  method='post'>
+        <input class='btn btn-primary' align='center' type='submit' value='Ir al perfil'>
+      </form>
+
+</body>
