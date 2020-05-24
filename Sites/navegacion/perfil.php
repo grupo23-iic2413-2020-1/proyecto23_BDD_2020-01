e2f6ae7afaf14fe5fae443eff7e1aa5a203c447f
Clemente Maruri <camaruri@uc.cl>
2020-05-24 11:40:53 -0400

    actualizacion reservas

--- Sites/navegacion/perfil.php
+++ Sites/navegacion/perfil.php
@@ -30,7 +30,6 @@ $result_2 = $db -> prepare($query_2);
 $result_2 -> execute();
 $entradas = $result_2 -> fetchAll();
 
-<<<<<<< HEAD
 $query_3 = "SELECT * FROM dblink('dbname=$databaseName_2' ,
             'SELECT Museo.lid, Lugar.lnombre, Museo.hora_apertura, Museo.hora_cierre FROM Museo, Lugar WHERE Museo.lid = Lugar.lid')
             AS t1(1lid INT, 1lnombre VARCHAR(255), 1hora_apertura TIME, 1hora_cierre TIME)";
@@ -52,8 +51,6 @@ $result_4 -> execute();
 $reservas = $result_4 -> fetchAll();
 
 
-=======
->>>>>>> refs/remotes/origin/master
 ?> 
 
 <body class= "bg-secondary text-white">
