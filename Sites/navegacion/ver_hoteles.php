8be6e90f0c6ac33ee0fe4b7974e2e451bb315c00
Clemente Maruri <camaruri@uc.cl>
2020-05-25 11:16:21 -0400

    ver hoteles lisito2

--- Sites/navegacion/ver_hoteles.php
+++ Sites/navegacion/ver_hoteles.php
@@ -33,7 +33,7 @@ $hoteles = $result -> fetchAll();
 
         <?php
             foreach ($hoteles as $htl) {
-            echo "<tr><td><a href='hotel_info.php?hid=$htl[0]&hnombre=$htl[1]&cnombre=$htl[2]'>$htl[1] $htl[2]</a></td></tr>";
+            echo "<tr><td><a href='hotel_info.php?hid=$htl[0]&hnombre=$htl[1]&cnombre=$htl[2]'>$htl[1]</a></td> <td> $htl[2] </td> </tr>";
             }
         ?>
         </tbody>
