bfa2b1c6b12ad0e8d59f4a3c1d36f33931261eff
Clemente Maruri <camaruri@uc.cl>
2020-05-25 11:33:25 -0400

    hotel info

---
+++ Sites/navegacion/hotel_info.php
@@ -0,0 +1,46 @@
+<?php session_start(); ?>
+<?php include('../templates/header.html');   ?>
+<?php include('../templates/navbar.php');   ?> 
+<body class= "bg-secondary text-white">
+<?php 
+# Muestra una tabla con todos los artistas
+
+  #Llama a conexión, crea el objeto PDO y obtiene la variable $db
+  require("../config/conexion.php");
+
+  $hid = $_GET['hid'];
+  $hnombre = $_GET['hnombre'];
+  $cnombre = $_GET['cnombre'];
+
+  #Se construye la consulta como un string
+  $query = "select * from hoteles
+            where $hid = hid;";
+	$result = $db -> prepare($query);
+	$result -> execute();
+  $hotel = $result -> fetchAll();
+
+?>
+<div class="row justify-content-md-center">
+            <h2> Informacion de <?php echo $hnombre;?> </h2>
+        </div>
+
+        <br>
+<div class="row justify-content-md-center">
+    <div class='col-md-auto'>
+        <div>
+            <h5><b>Nombre: </b> <?php echo $hnombre ?> </h5>
+        </div>
+        <div>
+            <h5><b>Ciudad: </b> <?php echo $cnombre ?> </h5>
+        </div>
+        <div>
+            <h5><b>Dirección: </b> <?php echo $hotel[3] ?> </h5>
+        </div>
+        <div>
+            <h5><b>Teléfono: </b> <?php echo $hotel[4] ?> </h5>
+        </div>
+        <div>
+            <h5><b>Precio: </b> <?php echo $hotel[5] ?> </h5>
+        </div>
+    </div>
+</div>
