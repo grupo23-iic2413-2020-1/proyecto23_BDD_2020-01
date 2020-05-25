1506af94bab08f02a1ba791baa16a8d994074fe3
Clemente Maruri <camaruri@uc.cl>
2020-05-25 11:04:37 -0400

    DFSFD

--- Sites/navegacion/ver_hoteles.php
+++ Sites/navegacion/ver_hoteles.php
@@ -6,45 +6,41 @@
 <?php 
 # Muestra una tabla con todos los artistas
 
-  #Llama a conexión, crea el objeto PDO y obtiene la variable $db
-  require("../config/conexion.php");
+#Llama a conexión, crea el objeto PDO y obtiene la variable $db
+require("../config/conexion.php");
 
-  #Se construye la consulta como un string
-  $query = "SELECT hnombre, cnombre FROM ciudades, hoteles where hoteles.cid = ciudades.cid order by cnombre";
+#Se construye la consulta como un string
+$query = "SELECT hnombre, cnombre FROM ciudades, hoteles where hoteles.cid = ciudades.cid order by cnombre;";
 
-  #Se prepara y ejecuta la consulta. Se obtienen TODOS los resultados
-  $result = $db_2 -> prepare($query);
-	$result -> execute();
-	$hoteles = $result -> fetchAll();
-  
-  ?>
-    <?php
-          foreach ($hoteles as $htl) {
-            echo "$htl[0] $htl[1]";
-          }
-        ?>
-  <div class="container">
+#Se prepara y ejecuta la consulta. Se obtienen TODOS los resultados
+$result = $db_2 -> prepare($query);
+$result -> execute();
+$hoteles = $result -> fetchAll();
+
+?>
+
+    <div class="container">
 
     <h1 class= "text-white" style="text-align: center; margin-top: 1rem"></h1>
 
     <table class="table table-bordered table-hover bg-white" style="align-self:center;width:90%;margin: 0 auto;">
 
-      <thead class="thead-dark">
+        <thead class="thead-dark">
         <tr style="text-align:center">
-          <th>Hotel</th>
-          <th>Ciudad</th>
+            <th>Hotel</th>
+            <th>Ciudad</th>
         </tr>
-      </thead>
-      <tbody>
+        </thead>
+        <tbody>
 
         <?php
-          foreach ($hoteles as $htl) {
-            echo "<tr> <td>$htl[0]</td>  <td>$htl[1]</td></tr>";
-          }
+            foreach ($hoteles as $htl) {
+            echo "<tr><td><a href='obra_info.php?oid=$obra[1]&onombre=$obra[0]'>$obra[0]</a></td></tr>";
+            }
         ?>
-      </tbody>
+        </tbody>
         
     </table>
-  </div>
+    </div>
 
 <?php include('../templates/footer.html'); ?>
