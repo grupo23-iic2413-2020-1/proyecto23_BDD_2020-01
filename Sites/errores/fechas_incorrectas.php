c0e3c31388f1d32557595ad50fc4c1e486c226e4
Clemente Maruri <camaruri@uc.cl>
2020-05-25 17:01:49 -0400

    fechas mal

---
+++ Sites/errores/fechas_incorrectas.php
@@ -0,0 +1,11 @@
+<?php session_start();
+include('../templates/header.html');   ?>
+<?php include('../templates/navbar.php');   ?> 
+<body class= "bg-secondary text-white">
+    <div class="container">
+        <div class="row justify-content-md-center">
+            <h2> Las fechas son invalidas </h2>
+        </div>
+    </div>
+</body>
+<?php include('../templates/footer.html');   ?>
