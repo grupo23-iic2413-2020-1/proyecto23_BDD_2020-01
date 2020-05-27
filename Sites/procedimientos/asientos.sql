d7effb0b0e30a990d36e733b52f03bd7a43563e8
Juan Pablo Correa <jpcorrea3@uc.cl>
2020-05-26 23:24:13 -0400

    dcdd

--- Sites/procedimientos/asientos.sql
+++ Sites/procedimientos/asientos.sql
@@ -1,17 +1,19 @@
 CREATE OR REPLACE FUNCTION
-seq (numero INTEGER)
-RETURNS TABLE (n integer) AS $$
+asientos (total_asientos INTEGER, dest_id INTEGER)
+RETURNS TABLE (asiento integer) AS $$
 DECLARE
 actual INTEGER;
 BEGIN
-CREATE TABLE secuencia(n integer);
+
+CREATE TABLE secuencia(asiento integer);
 actual = 1;
-WHILE numero >= actual LOOP
+WHILE total_asientos >= actual LOOP
 	INSERT INTO secuencia VALUES (actual);
 	actual = actual + 1;
 END LOOP;
-RETURN QUERY SELECT * FROM secuencia;
+RETURN QUERY SELECT * FROM (secuencia - (SELECT asiento FROM Tickets WHERE Tickets.did = dest_id ));
 DROP TABLE secuencia;
 END;
 $$ language plpgsql
+
 
