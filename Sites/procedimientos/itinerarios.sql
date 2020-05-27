692b54b5069d192bef60bb8ca2dc7ea16baeb914
Diego Fluxá <dgfluxa@uc.cl>
2020-05-27 19:10:47 -0400

    Arreglos itinerario

--- Sites/procedimientos/itinerarios.sql
+++ Sites/procedimientos/itinerarios.sql
@@ -41,15 +41,15 @@ BEGIN
 
     CREATE TABLE itinerarios (did1 integer, did2 integer DEFAULT NULL, did3 integer DEFAULT NULL);
 
-    FOR tupla in esc0 LOOP
+    FOR tupla in SELECT * FROM esc0 LOOP
         INSERT INTO TABLE itinerarios(did1, did2, did3) VALUES (tupla[0], NULL, NULL);
     END LOOP;
 
-    FOR tupla in esc1 LOOP
+    FOR tupla in SELECT * FROM esc1 LOOP
         INSERT INTO TABLE itinerarios(did1, did2, did3) VALUES (tupla[0], tupla[1], NULL);
     END LOOP;
 
-    FOR tupla in esc2 LOOP
+    FOR tupla in SELECT * FROM esc2 LOOP
         INSERT INTO TABLE itinerarios(did1, did2, did3) VALUES (tupla[0], tupla[1], tupla[2]);
     END LOOP;
 
