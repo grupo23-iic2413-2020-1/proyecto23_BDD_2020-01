942770edea222a105c65fd0939e72f370bf4536d
Juan Pablo Correa <jpcorrea3@uc.cl>
2020-05-27 00:16:36 -0400

    dd

--- Sites/navegacion/confirmacion_ticket.php
+++ Sites/navegacion/confirmacion_ticket.php
@@ -104,7 +104,7 @@ $asientos_disp = $result_3 -> fetchAll();
       echo 'fecha='.$fecha_pasaje.'&ciudad_origen='.$ciudad_origen.'&ciudad_destino='.$ciudad_destino.'&did='.$did ?>'  method='post'>
                 <div class="form-group">
                     <h5><b>Escoger asiento: </b> </h5>
-                    <select class="form-control form-control-lg" id="sel2" name='asiento' style='width: 7em  aling: center' >
+                    <select class="form-control form-control-lg" id="sel2" name='asiento' style='width: 7em; align: center' >
                     <?php
                     foreach ($asientos_disp as $asiento) {
                         echo "<option>$asiento[0]</option>";
