785ebe44d64a40054a514fa36c19d68e4abcd3c3
Diego Fluxá <dgfluxa@uc.cl>
2020-05-28 00:16:53 -0400

    Prueba mostrar itinerarios

--- Sites/navegacion/resultado_itinerario.php
+++ Sites/navegacion/resultado_itinerario.php
@@ -23,7 +23,7 @@ include('../templates/navbar.php');   ?>
 
   $cid = $ciudades[0][0];
 
-  $query2 = "SELECT * FROM itinerario(?,?,?);";
+  $query2 = "SELECT * FROM itinerario(?::INT[],?::integer,?::date);";
   $result = $db -> prepare($query2);
   $result -> bindParam(1, $artistas);
   $result -> bindParam(2, $cid);
