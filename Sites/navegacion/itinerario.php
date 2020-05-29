2e00b1872241e28bead0b1ca75335a516a99711b
felipe lira larrain <flira4@uc.cl>
2020-05-28 21:59:51 -0400

    itinerario

--- Sites/navegacion/itinerario.php
+++ Sites/navegacion/itinerario.php
@@ -37,8 +37,8 @@
        Elegir artistas:<br>
       <?php
         foreach ($artistas as $artista) {
-          echo "<label><input type='checkbox' style='width: 1em; height: 1em' name='artistas[]' 
-          value='$artista[1]'> $artista[0]</label><br>";
+          echo "<label><input type='checkbox' style='width: 1em; height: 1em' name='artistas[]'".
+          if($artista[0]==$artistas[0][0]){ echo " checked"}."value='$artista[1]'> $artista[0]</label><br>";
         }
       ?>
   </div>
--- Sites/navegacion/resultado_itinerario.php
+++ Sites/navegacion/resultado_itinerario.php
@@ -30,75 +30,65 @@ include('../templates/navbar.php');   ?>
   $result -> execute();
   $itinerarios = $result -> fetchAll(); ?>
 
-<?php if(empty($artistas)) { ?>
+
+
+<?php
+  echo "<h1>Resultados Creación de Itinerarios</h1><br>
+  <p>Fecha: $fecha  </p>
+  <p> Ciudad Inicial:  $ciudad </p>
+  <p>Id de Artistas: $artistas_str</p><br>
+  <h4> Itinerarios: </h4>";
+
+  $i = 1;
+  foreach ($itinerarios as $itinerario) { ?>
   <br>
-  <h1>Debes seleccionar un artista para tu itinerario</h1><br>
-  <br>
-  <form action="../index.php" method="get">
-    <input class="btn btn-success btn lg" type="submit" value="Volver">
-  </form>
-  <br>
-
-<?php } else { ?>
-
-  <?php
-    echo "<h1>Resultados Creación de Itinerarios</h1><br>
-    <p>Fecha: $fecha  </p>
-    <p> Ciudad Inicial:  $ciudad </p>
-    <p>Id de Artistas: $artistas_str</p><br>
-    <h4> Itinerarios: </h4>";
-
-    $i = 1;
-    foreach ($itinerarios as $itinerario) { ?>
-
-    <br>
-    <h5> Itinerario N° <?php echo $i ?>. Precio total = $<?php echo $itinerario[18] ?>
-      <table class="table table-bordered table-hover bg-white" style="align-self:center;width:90%;margin: 0 auto;">
-      <thead class="thead-dark">
-        <tr style="text-align:center">
-          <th>Ciudad Origen</th>
-          <th>Ciudad Destino</th>
-          <th>Medio</th>
-          <th>Hora Salida</th>
-          <th>Duración</th>
-          <th>Precio</th>
-        </tr>
-      </thead>
-      <tbody>
-      <tr>
-          <td><?php echo $itinerario[0] ?></td>
-          <td><?php echo $itinerario[1] ?></td>
-          <td><?php echo $itinerario[2] ?></td>
-          <td><?php echo $itinerario[3] ?></td>
-          <td><?php echo $itinerario[4] ?></td>
-          <td><?php echo $itinerario[5] ?></td>
+  <h5> Itinerario N° <?php echo $i ?>. Precio total = €<?php echo $itinerario[18] ?>
+    <table class="table table-bordered table-hover bg-white" style="align-self:center;width:90%;margin: 0 auto;">
+    <thead class="thead-dark">
+      <tr style="text-align:center">
+        <th>Ciudad Origen</th>
+        <th>Ciudad Destino</th>
+        <th>Medio</th>
+        <th>Hora Salida</th>
+        <th>Duración (hr)</th>
+        <th>Precio (€)</th>
       </tr>
-      <?php if ($itinerario[6] != NULL) { ?> 
-        <tr>
-          <td><?php echo $itinerario[6] ?></td>
-          <td><?php echo $itinerario[7] ?></td>
-          <td><?php echo $itinerario[8] ?></td>
-          <td><?php echo $itinerario[9] ?></td>
-          <td><?php echo $itinerario[10] ?></td>
-          <td><?php echo $itinerario[11] ?></td>
-        </tr>
-      <?php } ?>
-      <?php if ($itinerario[12] != NULL) {?> 
-        <tr>
-          <td><?php echo $itinerario[12] ?></td>
-          <td><?php echo $itinerario[13] ?></td>
-          <td><?php echo $itinerario[14] ?></td>
-          <td><?php echo $itinerario[15] ?></td>
-          <td><?php echo $itinerario[16] ?></td>
-          <td><?php echo $itinerario[17] ?></td>
-        </tr>
-      <?php } ?>
-      </tbody>
-      
-    </table>
-  <?php
-  $i = $i + 1;
-  }}
-  ?>
+    </thead>
+    <tbody>
+    <tr>
+        <td><?php echo $itinerario[0] ?></td>
+        <td><?php echo $itinerario[1] ?></td>
+        <td><?php echo $itinerario[2] ?></td>
+        <td><?php echo $itinerario[3] ?></td>
+        <td><?php echo $itinerario[4] ?></td>
+        <td><?php echo $itinerario[5] ?></td>
+    </tr>
+    <?php if ($itinerario[6] != NULL) { ?> 
+      <tr>
+        <td><?php echo $itinerario[6] ?></td>
+        <td><?php echo $itinerario[7] ?></td>
+        <td><?php echo $itinerario[8] ?></td>
+        <td><?php echo $itinerario[9] ?></td>
+        <td><?php echo $itinerario[10] ?></td>
+        <td><?php echo $itinerario[11] ?></td>
+      </tr>
+    <?php } ?>
+    <?php if ($itinerario[12] != NULL) {?> 
+      <tr>
+        <td><?php echo $itinerario[12] ?></td>
+        <td><?php echo $itinerario[13] ?></td>
+        <td><?php echo $itinerario[14] ?></td>
+        <td><?php echo $itinerario[15] ?></td>
+        <td><?php echo $itinerario[16] ?></td>
+        <td><?php echo $itinerario[17] ?></td>
+      </tr>
+    <?php } ?>
+    </tbody>
+    
+  </table>
+<?php
+$i = $i + 1;
+}
+?>
 
 <?php include('../templates/footer.html'); ?>
