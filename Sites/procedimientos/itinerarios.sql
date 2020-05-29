b38bb9e9991392f321d25c5b6622d610a00b6ddb
Diego Fluxá <dgfluxa@uc.cl>
2020-05-29 15:41:09 -0400

    Prueba manejo de fechas itinerario

--- Sites/procedimientos/itinerarios.sql
+++ Sites/procedimientos/itinerarios.sql
@@ -69,11 +69,17 @@ BEGIN
 
     RETURN QUERY 
     SELECT DISTINCT itinerarios.cnombre11, itinerarios.cnombre12, d1.medio, d1.salida, d1.duracion, d1.precio, fecha as fecha_d1,
-    itinerarios.cnombre21, itinerarios.cnombre22, d2.medio, d2.salida, d2.duracion, d2.precio, fecha as fecha_d2,
-    itinerarios.cnombre31, itinerarios.cnombre32, d3.medio, d2.salida, d3.duracion, d3.precio, fecha as fecha_d3,
+    itinerarios.cnombre21, itinerarios.cnombre22, d2.medio, d2.salida, d2.duracion, d2.precio, 
+    CASE fecha_d2 
+    WHEN d1.salida + + interval '1h' * d1.duracion > d2.salida 
+    THEN fecha_d1  + interval '1 day' ELSE fecha_d1 
+    END,
+    itinerarios.cnombre31, itinerarios.cnombre32, d3.medio, d2.salida, d3.duracion, d3.precio, 
+    CASE fecha_d3 
+    WHEN d2.salida + + interval '1h' * d2.duracion > d3.salida 
+    THEN fecha_d2  + interval '1 day' ELSE fecha_d2 
+    END,
     (d1.precio + d2.precio + d3.precio) as precio_total
-    CASE fecha_d2 WHEN d1.salida + + interval '1h' * d1.duracion > d2.salida THEN fecha_d2 = fecha_d1  + interval '1 day' ELSE fecha_d2 = fecha_d1 END AS fecha_d2
-    CASE fecha_d3 WHEN d2.salida + + interval '1h' * d2.duracion > d3.salida THEN fecha_d3 = fecha_d2  + interval '1 day' ELSE fecha_d3 = fecha_d2 END AS fecha_d3
     FROM itinerarios, Destinos as d1, Destinos as d2, Destinos as d3
     WHERE itinerarios.did1 = d1.did
     AND itinerarios.did2 = d2.did
@@ -82,7 +88,11 @@ BEGIN
     UNION
 
     SELECT DISTINCT itinerarios.cnombre11, itinerarios.cnombre12, d1.medio, d1.salida, d1.duracion, d1.precio, fecha as fecha_d1,
-    itinerarios.cnombre21, itinerarios.cnombre22, d2.medio, d2.salida, d2.duracion, d2.precio, fecha as fecha_d2,
+    itinerarios.cnombre21, itinerarios.cnombre22, d2.medio, d2.salida, d2.duracion, d2.precio, 
+    CASE fecha_d2 
+    WHEN d1.salida + + interval '1h' * d1.duracion > d2.salida 
+    THEN fecha_d1  + interval '1 day' ELSE fecha_d1 
+    END,
     NULL as cnombre1_d3, NULL as cnombre2_d3, NULL as medio_d3, NULL::time as hora_d3, NULL::double precision as duracion_d3, NULL::integer as precio_d3, NULL::date as fecha_d3,
     (d1.precio + d2.precio)
     CASE fecha_d2 WHEN d1.salida + + interval '1h' * d1.duracion > d2.salida THEN fecha_d2 = fecha_d1  + interval '1 day' ELSE fecha_d2 = fecha_d1 END AS fecha_d2
