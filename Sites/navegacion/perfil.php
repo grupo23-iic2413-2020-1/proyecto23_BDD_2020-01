af95f70d28adf9ed766976996b19dfc1663bd732
felipe lira larrain <flira4@uc.cl>
2020-05-29 00:36:37 -0400

    DATES

--- Sites/navegacion/perfil.php
+++ Sites/navegacion/perfil.php
@@ -48,7 +48,8 @@ $result_4 = $db -> prepare($query_4);
 $result_4 -> execute();
 $reservas = $result_4 -> fetchAll();
 
-$query_5 = "select asiento, fechac, fechav, c1.cnombre, c2.cnombre
+$query_5 = "select asiento, CONVERT(date, fechac) AS 'fechac'
+            , CONVERT(date, fechav) AS 'fechac', c1.cnombre, c2.cnombre
             from tickets, destinos, ciudades as c1, ciudades as c2
             where uid = $uid
             and tickets.did = destinos.did 
@@ -109,8 +110,7 @@ $tickets = $result_5 -> fetchAll();
 
                         <?php
                             foreach ($tickets as $tik) {
-                            echo "<tr> <td>$tik[0]</td> <td>".date($tik[1], 'Y-m-d')."</td> <td>".date($tik[2], 'Y-m-d').
-                                 "</td> <td>$tik[3]</td> <td>$tik[4]</td></tr>";
+                            echo "<tr> <td>$tik[0]</td> <td>$tik[1]</td> <td>$tik[2]</td> <td>$tik[3]</td> <td>$tik[4]</td></tr>";
                         }
                         ?>
                         </tbody>
