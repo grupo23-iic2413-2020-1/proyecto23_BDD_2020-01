bf82ec6592cc88eb16204bb97c048192e3db0a59
felipe lira larrain <flira4@uc.cl>
2020-05-28 23:08:27 -0400

    image

---
+++ Sites/navegacion/simple_html_dom.php
@@ -0,0 +1,13 @@
+<?php
+
+$html = file_get_html('http://www.google.com/');
+
+// Find all images
+foreach($html->find('img') as $element)
+       echo $element->src . '<br>';
+
+// Find all links
+foreach($html->find('a') as $element)
+       echo $element->href . '<br>';
+
+?>
