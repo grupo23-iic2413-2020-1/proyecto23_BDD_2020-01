97e6ea5e5b82d5f78c9df2c875cd1b3b6ec5ef86
felipe lira larrain <flira4@uc.cl>
2020-05-28 23:05:50 -0400

    artista foto

--- Sites/navegacion/artista_info.php
+++ Sites/navegacion/artista_info.php
@@ -65,7 +65,22 @@
 
 
   <br>
-
+  <?php
+    include "simple_html_dom.php";
+    $search_query = $artista[1];
+    $search_query = urlencode( $search_query );
+    $html = file_get_html( "https://www.google.com/search?q=$search_query&tbm=isch" );
+    $image_container = $html->find('div#rcnt', 0);
+    $images = $image_container->find('img');
+    $image_count = 1; //Enter the amount of images to be shown
+    $i = 0;
+    foreach($images as $image){
+        if($i == $image_count) break;
+        $i++;
+        // DO with the image whatever you want here (the image element is '$image'):
+        echo $image;
+    }
+  ?>
 
   <br>
 	<div class="container">
