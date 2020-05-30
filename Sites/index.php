d443ce1df39fc4c3abf362b79562e479ab420070
felipe lira larrain <flira4@uc.cl>
2020-05-29 22:42:41 -0400

    K

--- Sites/index.php
+++ Sites/index.php
@@ -78,7 +78,7 @@ include('templates/navbar.php');
                             </p>
                         </div>
                         <div class="col-12 col-lg-4 col-md-6 text-center">
-                            <img src="imagenes_perfil/pelao.jpg" class="rounded-circle" alt="Pela3">
+                            <img src="imagenes_perfil/pelao.jpg" alt="" class="mx-auto rounded-circle img-fluid">
                             <br>
                             <ul class="list-inline ratings text-center" title="Ratings">
                                 <li class="list-inline-item"><a href="#"><span class="fa fa-star"></span></a>
