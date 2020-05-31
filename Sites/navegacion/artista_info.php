d68ebe6b3a347e5b67850d7669f6d9644a45e57c
felipe lira larrain <flira4@uc.cl>
2020-05-31 16:21:05 -0400

    Imagenes artistas

--- Sites/navegacion/artista_info.php
+++ Sites/navegacion/artista_info.php
@@ -69,23 +69,39 @@
   <br>
 
 <?php
-  $nombre_busqueda = $artistas[0][1];
+  $busqueda = $artistas[0][1]
 
-  $sURL = "https://api.cognitive.microsoft.com/bing/v7.0/images/search?q=$nombre_busqueda";
-  $key = "caf911e140684520b515eaefe37af2e8";
+  $accessKey = 'caf911e140684520b515eaefe37af2e8';
+  $endpoint = 'https://api.cognitive.microsoft.com/bing/v7.0/images';
 
-  $ch = curl_init();
-  curl_setopt($ch, CURLOPT_URL, $sURL); 
-  curl_setopt($ch, CURLOPT_TIMEOUT, '1'); 
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-  curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-      'Content-Type: multipart/form-data',
-      'Ocp-Apim-Subscription-Key: caf911e140684520b515eaefe37af2e8'
-  ));
-  $content = curl_exec($ch);
-  
-  echo $content;
+  $headers = "Ocp-Apim-Subscription-Key: $accessKey\r\n";
+    $options = array ('http' => array (
+                          'header' => $headers,
+                           'method' => 'GET'));
+
+    // Perform the request and get a JSON response.
+    $context = stream_context_create($options);
+    $resultado = file_get_contents($endpoint . "?q=" . urlencode($busqueda), false, $context);
+    echo $resultado;
+    // Extract Bing HTTP headers.
+    $headers = array();
+    foreach ($http_response_header as $k => $v) {
+        $h = explode(":", $v, 2);
+        if (isset($h[1]))
+            if (preg_match("/^BingAPIs-/", $h[0]) || preg_match("/^X-MSEdge-/", $h[0]))
+                $headers[trim($h[0])] = trim($h[1]);
+    }
+
+    list($headers, $json) = array($headers, $resultado);
+
+    print "\nRelevant Headers:\n\n";
+    foreach ($headers as $k => $v) {
+        print $k . ": " . $v . "\n";
+    }
+    // Prints JSON encoded response.
+    print "\nJSON Response:\n\n";
+    echo $json;
+    echo json_encode(json_decode($json), JSON_PRETTY_PRINT);
 
 
 ?>
