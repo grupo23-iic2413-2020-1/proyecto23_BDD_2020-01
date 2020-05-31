a5d01de5b9a4eb1f8db2285fcea0e8a70aec0ecc
felipe lira larrain <flira4@uc.cl>
2020-05-31 16:55:25 -0400

    Imagenes artistas

--- Sites/navegacion/artista_info.php
+++ Sites/navegacion/artista_info.php
@@ -96,7 +96,7 @@
     // echo json_encode(json_decode($json), JSON_PRETTY_PRINT);
     //echo json_encode(json_decode($json), JSON_PRETTY_PRINT);
 
-    echo $headers;
+    echo $headers['value']['contentUrl'];
 
     $json = json_encode(json_decode($json), true);
     echo $json['value']['contentUrl'].'<br><br><br><br>';
