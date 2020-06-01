c4641b5d32d28fb3179a9c524a3f614c8873273d
Diego Fluxá <dgfluxa@uc.cl>
2020-06-01 14:38:21 -0400

    Prueba mejora rutas navbar

--- Sites/templates/navbar.php
+++ Sites/templates/navbar.php
@@ -1,5 +1,5 @@
 <?php session_start();
-$base_url = $_SESSION["base_url"];?> 
+include("config/url_base.php");?> 
 <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
   <a class="navbar-brand text-success" href="<?php echo $base_url ?>/index.php"><b>Splinter S.A.</b></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
