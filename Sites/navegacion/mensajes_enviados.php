b992f17e4f453a08ee3a2e2927e50372d1196132
Juan Pablo Correa <jpcorrea3@uc.cl>
2020-06-29 22:49:24 -0400

    scscfbbb

--- Sites/navegacion/mensajes_enviados.php
+++ Sites/navegacion/mensajes_enviados.php
@@ -3,13 +3,12 @@
 <?php include('../templates/navbar.php');   ?> 
 
 <?php 
-require_once __DIR__ . '/vendor/autoload.php';
-
-use HerokuClient\Client as HerokuClient;
-
 $heroku = new HerokuClient([
-    'apiKey' => 'https://entrega5-2350-api-heroku.herokuapp.com/', // Or set the HEROKU_API_KEY environmental variable
-]);
+                            'baseUrl' => 'https://entrega5-2350-api-heroku.herokuapp.com/',   // Defaults to https://api.heroku.com/
+                            ],
+                          ]);
+
+
 
 $users = $heroku->get('users')->quantity;
 
