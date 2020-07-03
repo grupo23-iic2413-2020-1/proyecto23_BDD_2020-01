9cc5ad1663ba714de3fbe5ddfad8d09de42cea7d
Clemente Maruri <camaruri@uc.cl>
2020-07-03 18:24:02 -0400

    vista text search

---
+++ Sites/navegacion/text_search.php
@@ -0,0 +1,22 @@
+<?php session_start();
+include('../templates/header.html');
+include('../templates/navbar.php');  
+require("../config/conexion.php"); 
+
+$url = "https://nameless-meadow-87804.herokuapp.com/messages";
+$json = file_get_contents($url);
+$json_data = json_decode($json, true);?>
+<body class= "bg-secondary text-white">
+  <div class='row'>
+    <div class='col-xs'>
+      <?php include('../templates/vertical_navbar.php');  ?>
+    </div>
+    <div class="container">
+      <div class='col-no-gutters'>
+        <h1 class= "text-white" style="text-align: center; margin-top: 1rem">Buscar Mensajes</h1>
+
+        <?php include('../templates/footer.html'); ?>
+      </div>
+    </div>
+  </div>
+</body>
