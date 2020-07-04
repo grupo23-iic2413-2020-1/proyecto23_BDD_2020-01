31a23ff931813a77eae95a3ca8af60c35d51502b
felipe lira larrain <flira4@uc.cl>
2020-07-04 19:35:46 -0400

    Enviar mensajes

---
+++ Sites/navegacion/confirmacion_mensaje.php
@@ -0,0 +1,107 @@
+<?php session_start(); ?>
+<?php include('../templates/header.html');   ?>
+<?php include('../templates/navbar.php');  
+require("../config/conexion.php"); ?>
+
+<?php 
+
+$receptor = $_POST["user"];
+$contenido = $_POST["contenido"];
+
+$new_arr[]= unserialize(file_get_contents('http://www.geoplugin.net/php.gp?ip='.$_SERVER['REMOTE_ADDR']));
+
+$fecha = date('Y-m-d');
+
+$url = "https://nameless-meadow-87804.herokuapp.com/messages";
+
+$query = "SELECT * FROM Usuarios WHERE Usuarios.uid = ?";
+
+#Se prepara y ejecuta la consulta. Se obtienen TODOS los resultados
+$result = $db -> prepare($query);
+$result -> bindParam(1, $_SESSION['current_uid']);
+$result -> execute();
+$user = $result -> fetchAll();
+
+$uid = $user[0][0];
+
+$query_2 = "SELECT * FROM Usuarios WHERE Usuarios.unombre = $receptor";
+
+#Se prepara y ejecuta la consulta. Se obtienen TODOS los resultados
+$result_2 = $db -> prepare($query_2);
+$result_2 -> execute();
+$user_receptor = $result_2 -> fetchAll();
+
+
+
+
+?>
+
+<body class= "bg-secondary text-white">
+  <div class='row'>
+    <div class='col-xs'>
+      <?php include('../templates/vertical_navbar.php');  ?>
+    </div>
+    
+    <div class="container">
+      <div class='col-no-gutters'>
+      <?php if(empty($user_receptor)) { ?>
+          <h1 class= "text-white" style="text-align: center; margin-top: 1rem">No existe usuario ingresado</h1>
+
+      <?php } else { 
+        $uid_receptor = $user_receptor[0][0];
+        
+        $data = array(
+          'date'      => $fecha,
+          'lat'    => $new_arr[0]['geoplugin_latitude'],
+          'long'       => $new_arr[0]['geoplugin_longitude'],
+          'message' => $contenido,
+          'receptant'      => $uid_receptor,
+          'sender'      => $uid
+        );
+
+        $options = array(
+          'http' => array(
+            'method'  => 'POST',
+            'content' => json_encode( $data ),
+            'header'=>  "Content-Type: application/json\r\n" .
+                        "Accept: application/json\r\n"
+            )
+        );
+        
+        $context  = stream_context_create( $options );
+        $result = file_get_contents( $url, false, $context );
+        $response = json_decode( $result );
+
+
+        
+        ?>
+
+        <h1 class= "text-white" style="text-align: center; margin-top: 1rem"><?php $response ?></h1>
+        <table class="table table-bordered table-hover bg-white" style="align-self:center;width:90%;margin: 0 auto;">
+        <thead class="thead-dark">
+                <tr style="text-align:center">
+                <th>Id Mensaje</th>
+                <th>Destinatario</th>
+                <th>Fecha</th>
+                <th>Latitud</th>
+                <th>Longitud</th>
+                <th>Contenido</th>
+                </tr>
+              </thead>
+              <tbody>
+        <?php 
+          echo '<tr><td>'.$mid.'</a></td>';
+          echo '<td>'.$uid_receptor.'</a></td>';
+          echo '<td>'.$fecha.'</a></td>';
+          echo '<td>'.$new_arr[0]['geoplugin_latitude'].'</a></td>';
+          echo '<td>'.$new_arr[0]['geoplugin_longitude'].'</a></td>';
+          echo '<td>'.$contenido.'</a></td></tr>';
+        }
+        ?>
+        </table>
+      <?php } ?>
+      </div>
+    </div>
+  </div>
+</body>
+<?php include('../templates/footer.html'); ?>
--- Sites/navegacion/enviar_mensaje.php
+++ Sites/navegacion/enviar_mensaje.php
@@ -16,20 +16,17 @@ $json_data = json_decode($json, true);?>
         <h1 class= "text-white" style="text-align: center; margin-top: 1rem">Enviar un nuevo mensaje</h1>
         <h2 class= "text-white" style="text-align: center; margin-top: 1rem">Ingrese las características del mensaje</h2>
 
-        <form align="center" action="#" method="post">
+        <form align="center" action="confirmacion_mensaje.php" method="post">
         Usuario destinatario:
         <input class="w-25" type="text" name="user">
         <br/><br/>
         Contenido mensaje:
-        <textarea placeholder="Escribe aquí tu mensaje..." name="texto" cols="40" rows="10"></textarea>
+        <br/>
+        <textarea placeholder="Escribe aquí tu mensaje..." name="contenido" cols="40" rows="10"></textarea>
         <br/><br/>
         <input class="btn btn-primary" type="submit" value="Enviar">
         <?php include('../templates/footer.html'); ?>
       </div>
     </div>
   </div>
-  <?php
-  $new_arr[]= unserialize(file_get_contents('http://www.geoplugin.net/php.gp?ip='.$_SERVER['REMOTE_ADDR']));
-  echo "Latitude:".$new_arr[0]['geoplugin_latitude']." and Longitude:".$new_arr[0]['geoplugin_longitude'];
-  ?>
 </body>
