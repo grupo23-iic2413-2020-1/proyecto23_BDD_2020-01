75e4ca77288126d97df1bab6f83cb181a9480078
Clemente Maruri <camaruri@uc.cl>
2020-07-06 08:15:14 -0400

    prueba

--- Sites/navegacion/mensajeria.php
+++ Sites/navegacion/mensajeria.php
@@ -8,7 +8,7 @@ require("../config/conexion.php");
 
 
 if ($_SESSION['loggedin'] == False) {
-    header("location: ../errores/perfil1.php");
+    header("location: ../errores/mensajes_login.php");
     exit;} 
 
 
