f83f87776931bc3541fd475d31b723931cbe1c4c
Juan Pablo Correa <jpcorrea3@uc.cl>
2020-07-06 22:14:07 -0400

    arreglo text-search

--- Sites/navegacion/text_search.php
+++ Sites/navegacion/text_search.php
@@ -18,7 +18,7 @@ $json_data = json_decode($json, true);?>
 
         <form align="center" action="resultados_text_search.php" method="post">
         Requerido:
-        <input class="form-control-40" type="text" name="required">
+        <input class="form-control-100" type="text" name="required">
         <br/><br/>
         Deseado:
         <input class="form-control" type="text" name="desired">
