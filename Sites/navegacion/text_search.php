3c6715ae5df164baaa037f43f062cf903b74e395
Juan Pablo Correa <jpcorrea3@uc.cl>
2020-07-06 22:34:46 -0400

    arreglo text-search

--- Sites/navegacion/text_search.php
+++ Sites/navegacion/text_search.php
@@ -8,65 +8,6 @@ $json = file_get_contents($url);
 $json_data = json_decode($json, true);?>
 <body class= "bg-secondary text-white">
 
-  <div class="container-lg">
-      <div class="table-responsive">
-          <div class="table-wrapper">
-              <div class="table-title">
-                  <div class="row">
-                      <div class="col-sm-8"><h2>Employee <b>Details</b></h2></div>
-                      <div class="col-sm-4">
-                          <button type="button" class="btn btn-info add-new"><i class="fa fa-plus"></i> Add New</button>
-                      </div>
-                  </div>
-              </div>
-              <table class="table table-bordered">
-                  <thead>
-                      <tr>
-                          <th>Name</th>
-                          <th>Department</th>
-                          <th>Phone</th>
-                          <th>Actions</th>
-                      </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                        <td>John Doe</td>
-                        <td>Administration</td>
-                        <td>(171) 555-2222</td>
-                        <td>
-                            <a class="add" title="Add" data-toggle="tooltip"><i class="material-icons">&#xE03B;</i></a>
-                            <a class="edit" title="Edit" data-toggle="tooltip"><i class="material-icons">&#xE254;</i></a>
-                            <a class="delete" title="Delete" data-toggle="tooltip"><i class="material-icons">&#xE872;</i></a>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Peter Parker</td>
-                        <td>Customer Service</td>
-                        <td>(313) 555-5735</td>
-                        <td>
-                        <a class="add" title="Add" data-toggle="tooltip"><i class="material-icons">&#xE03B;</i></a>
-                            <a class="edit" title="Edit" data-toggle="tooltip"><i class="material-icons">&#xE254;</i></a>
-                            <a class="delete" title="Delete" data-toggle="tooltip"><i class="material-icons">&#xE872;</i></a>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Fran Wilson</td>
-                        <td>Human Resources</td>
-                        <td>(503) 555-9931</td>
-                        <td>
-                            <a class="add" title="Add" data-toggle="tooltip"><i class="material-icons">&#xE03B;</i></a>
-                            <a class="edit" title="Edit" data-toggle="tooltip"><i class="material-icons">&#xE254;</i></a>
-                            <a class="delete" title="Delete" data-toggle="tooltip"><i class="material-icons">&#xE872;</i></a>
-                        </td>
-                    </tr>      
-  
-                  </tbody>
-            </table>
-        </div>
-    </div>
-</div>    
-
-
   <div class='row'>
     <div class='col-xs'>
       <?php include('../templates/vertical_navbar.php');  ?>
